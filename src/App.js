1b3d653a50fd851871c79457fc08c9d7f8d6edb7
Sean McIntyre <seanmcintyre@gmail.com>
2018-05-13 20:57:04 -0400

    how do you steer this thing???

--- src/App.js
+++ src/App.js
@@ -2,17 +2,86 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const skyline = () => 'foo';
+const nextHour = (hour) => hour + 1 < 24 ? { hour: hour + 1 } : { hour: 0 };
+
+class Clock extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      hour: 10
+    };
+  }
+
+  nextHour = () => {
+    this.setState(prevState => nextHour(this.state.hour));
+  }
+
+  render() {
+    return (
+      <div>
+        <svg className="clock">
+          {/*ng6: [style.stroke-dasharray]="clock.getDash()">*/}
+            <circle id="circle" r="37.5" cx="75" cy="75"></circle>
+        </svg>
+
+        <span className="hour"
+          onClick={this.nextHour}>
+          {this.state.hour}
+        </span>
+
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Clock></Clock>
+
+        <main>
+          <h1>Sean McIntyre</h1>
+          <span className="discipline">Product Designer, Engineer</span>
+
+          {/*<about></about>*/}
+
+          <div className="stats">
+            <a className="resume"
+              href="assets/resume.pdf"
+              target="_blank">
+              <div className="icon">👩🏻‍🎤</div>
+              <em>download my resume</em>
+            </a>
+
+            <a className="portfolio"
+              href="mailto:hello@smcntyr.com"
+              target="_blank">
+              <div className="icon">🎨</div>
+              <em>contact for my portfolio</em>
+            </a>
+
+            <span className="current">
+              <em>current tech interests</em><br />
+              <span>TypeScript</span>
+              <span>GraphQL</span>
+              <span>Angular 6</span>
+              <span>React</span>
+              <span>Web Animation</span>
+              <span>SVG</span>
+              <span>CSS Grid</span>
+              <span>PostCSS</span>
+              <span>RxJS</span>
+              <span>Progressive Web Apps</span>
+            </span>
+          </div>
+        </main>
+
+        <img className="skyline"
+          src={skyline()}
+          alt="" />
       </div>
     );
   }
